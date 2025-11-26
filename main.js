d9700448ae30a8df1f519497613f09e5d674f722
Константин-1122 <hamskijham331@gmail.com>
2025-11-26 21:08:54 +0300

    first commit1

---
+++ main.js
@@ -0,0 +1,23 @@
+
+let result1 = (5 + 3) * 2 - 7;
+console.log(`operation1 * ${result1}`);
+
+let result2 = (10 / 2) ** 2;
+console.log(`operation2 ** ${result2}`);
+
+let result3 = 2 * (4 + 3) - (8 / 2);
+console.log(`operation * - ${result3}`);
+
+let result4 = (15 % 4) + 5;
+console.log(`operation % ${result4} `);
+
+let result5 = 3 - (2 ** 4);
+console.log(`operation ** ${result5}`);
+
+
+// primery
+
+let result6 = (4 ** (1/2));
+console.log(`operation ** ${result6}`);
+let result7 = (8 % (1/3)); 
+console.log(`operation % ${result7}`);
